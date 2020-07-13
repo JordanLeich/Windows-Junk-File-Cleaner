8f5f04ab829a3a40e140266a5439c29b931da2a3
Jordan Leich <jordanleich@gmail.com>
2020-07-13 19:51:12 -0400

    Update 5
    Cleaner now outputs what files were deleted in real-time
    Cleaner now shows how many total files were deleted

--- Cleaner.py
+++ Cleaner.py
@@ -27,6 +27,7 @@ def second():
     time.sleep(2)
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
+        print(good_color + "Deleted " + filename, '\n')
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
@@ -34,6 +35,10 @@ def second():
                 shutil.rmtree(file_path)
         except Exception as e:
             print(bad_color + 'Failed to delete %s. Reason: %s' % (file_path, e), '\n')
+            time.sleep(1)
+            junk_removed = len(folder)
+            print(good_color + 'Total junk files deleted in the first folder:', junk_removed, '\n')
+            time.sleep(2)
             third()
 
 
@@ -43,6 +48,7 @@ def third():
     time.sleep(2)
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
+        print(good_color + "Deleted " + filename, '\n')
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
@@ -51,6 +57,9 @@ def third():
         except Exception as e:
             print(bad_color + 'Failed to delete %s. Reason: %s' % (file_path, e), '\n')
             time.sleep(1)
+            junk_removed = len(folder)
+            print(good_color + 'Total junk files deleted in the second folder:', junk_removed, '\n')
+            time.sleep(2)
             end()
 
 
