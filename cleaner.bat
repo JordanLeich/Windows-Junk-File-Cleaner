7e23535f4418b75a526ebc50840d21a45c57800d
Jordan Leich <jordanleich@gmail.com>
2022-07-28 03:03:54 -0400

    Small batch upgrades

--- cleaner.bat
+++ cleaner.bat
@@ -3,8 +3,9 @@
 title Windows Junk File Cleanup
 color 0a
 
-REM Removing junk files.
-echo Removing junk files . . .
+REM Removing all junk files.
+echo Removing all junk files . . .
+echo.
 
 del "C:\Windows\Prefetch\*.*" /s /f /q
 del %temp%\*.* /s /f /q
@@ -14,5 +15,8 @@ del "C:\Windows\Downloaded Program Files\*.*" /s /f /q
 del "\$Recycle.Bin\%SID%\*.*" /s /f /q
 
 REM Finished.
+echo.
 echo All junk files removed!
+echo.
+pause
 
