d76c9ec01d0f22f012fe5795d6fb23317532f80b
Jordan Leich <jordanleich@gmail.com>
2022-07-30 00:58:11 -0400

    Batch upgrades

--- cleaner.bat
+++ cleaner.bat
@@ -3,25 +3,29 @@
 title Windows Junk File Cleanup
 color 0a
 
-REM Removing all junk files.
-echo Removing all junk files . . .
+echo Running windows built in Disk Cleanup tool . . .
 echo.
 
+REM Running windows built in Disk Cleanup tool.
+START cleanmgr.exe /sagerun:1
+pause
+echo. 
+
+echo Removing more junk folders and files . . .
+echo.
+REM Removing more junk folders and files that the Disk Cleanup tool does not clean.
 rd "%temp%" /s /q
 rd "%WINDIR%\Temp" /s /q
-rd "%TMP%" /s /q
+rd "%WINDIR%\Prefetch" /s /q
 rd "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\IE" /s /q
 
 del "%WINDIR%\Downloaded Program Files\*" /s /f /q
-
-cd /d %WINDIR%\Prefetch
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
 for /f "skip=1" %%d in ('wmic logicaldisk get DeviceID ^| findstr /v /r "^$"') do for /f "delims=" %%u in ('dir /a /b %%d\$Recycle.bin 2^>nul') do rd "%%d\$Recycle.bin\%%u" /s /q
 
 REM Finished.
 echo.
-echo All junk files removed!
+echo All junk folders and files removed!
 echo.
 pause
 
